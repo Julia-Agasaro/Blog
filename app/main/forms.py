c7fb34e7b37384b34e0217cc8a5fd2958834034a
Julia Agasaro <agasarojulia@gmail.com>
2019-09-27 09:13:19 +0200

    Added a form for comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -16,9 +16,3 @@ class CommentForm(FlaskForm):
 	description = TextAreaField('Add comment',validators=[Required()])
 	submit = SubmitField()
 
-class UpvoteForm(FlaskForm):
-	submit = SubmitField()
-
-
-class DownvoteForm(FlaskForm):
-	submit = SubmitField()
