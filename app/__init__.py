422b53be8a0d809c0240eeec403cabf164f511a5
Julia Agasaro <agasarojulia@gmail.com>
2019-09-29 23:09:27 +0200

    Modified init.py

--- app/__init__.py
+++ app/__init__.py
@@ -40,7 +40,7 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/auth')
-
+    
     # # setting config
     # from .request import configure_request
     # configure_request(app)
