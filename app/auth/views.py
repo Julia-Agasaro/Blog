73c6ff5b2245f2791c15cde9e4cb289b4275f571
Julia Agasaro <agasarojulia@gmail.com>
2019-09-30 12:24:58 +0200

    Added a subscribe function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
-from ..models import User
-from .forms import LoginForm,RegistrationForm
+from ..models import User,Subscription
+from .forms import LoginForm,RegistrationForm,SubscriptionForm
 from .. import db
 from ..email import mail_message
 
@@ -40,3 +40,16 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/subscription', methods=["GET", "POST"])
+def subscription():
+   subform = SubscriptionForm()
+   if subform.validate_on_submit():
+       subscribers = Subscription(name=subform.name.data, email=subform.user_email.data)
+       db.session.add(subscribers)
+       db.session.commit()
+       mail_message("Welcome to Express Yourself Blog...",
+                    "email/welcome_user", subscribers.email, subscribers=subscribers)
+       return redirect(url_for('main.index'))
+       title = "New Subscription"
+   return render_template('auth/subscribe.html', subscription_form=subform)
